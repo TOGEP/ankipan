51964f83ef2850299a3f7483692541e6ef58c975
k-nasa <htilcs1115@gmail.com>
2019-04-09 15:52:32 +0900

    feat: Add migration create_cards table

---
+++ db/migrations/20190409154811-create_cards.sql
@@ -0,0 +1,13 @@
+
+-- +migrate Up
+create table cards (
+  id bigint unsigned auto_increment not null primary key,
+  user_id bigint unsigned not null,
+  problem_statement text not null,
+  answer_text text not null,
+  memo text not null,
+  question_time datetime not null
+);
+
+-- +migrate Down
+drop table if exists cards;
