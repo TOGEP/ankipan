37a390557faf2bddb4cfdd0257a6af4513fa70ea
TOGEP <sg.endo0302@gmail.com>
2019-04-17 09:17:36 +0900

    merged migration

--- main.go
+++ main.go
@@ -1,29 +1,64 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"path/filepath"
-	"sync"
-	"text/template"
+
+	"./models"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/labstack/echo"
 )
 
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
-}
-
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-	t.templ.Execute(w, nil)
+type Template struct {
+	templates *template.Template
 }
 
 func main() {
-	http.Handle("/", &templateHandler{filename: "chat.html"})
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe:", err)
+	/*  db, err := sql.Open("mysql", "root:@/ankipan_card")
+	      if err != nil {
+	      panic(err.Error())
+	    }
+	    defer db.Close()*/
+
+	e := echo.New()
+	e.File("/", "views/page1.html")
+	e.POST("/create", CreateCard)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func CreateCard(c echo.Context) error {
+	db, err := sql.Open("mysql", "root:@/ankipan_card")
+	if err != nil {
+		panic(err.Error())
 	}
+	defer db.Close()
+
+	problem := c.FormValue("problem")
+	anser := c.FormValue("anser")
+	note := c.FormValue("note")
+
+	card := models.Card{Problem: problem, Anser: anser, Note: note}
+
+	query := "INSERT INTO user values(null,?,?,?,?)"
+	result, err := db.Exec(query, "card", "problem", "anser", "note")
+	if err != nil {
+		panic(err.Error())
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if rows != 1 {
+		log.Fatalf("expected to affect 1 row, affected %d", rows)
+	}
+
+	fmt.Println(card)
+	fmt.Println(problem)
+	fmt.Println(anser)
+	fmt.Println(note)
+	return c.JSON(http.StatusOK, card)
 }
