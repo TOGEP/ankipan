69b7e78a9b483dee0e5e4bf382a1f9742c1176ea
TOGEP <sg.endo0302@gmail.com>
2019-04-29 17:38:03 +0900

    add CreateUser function

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo"
 )
+
 var db *sql.DB
 var err error
 
@@ -15,21 +16,21 @@ func main() {
 	e := echo.New()
 
 	e.POST("/create", CreateCard)
-  e.POST("/user", CreateUser)
+	e.POST("/user", CreateUser)
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 func getDB() {
 
-  db, err = sql.Open("mysql", "root:@/ankipan")
-  if err !=nil{
-    panic(err.Error())
-  }
-  return
+	db, err = sql.Open("mysql", "root:@/ankipan")
+	if err != nil {
+		panic(err.Error())
+	}
+	return
 }
 
 func CreateCard(c echo.Context) error {
-  getDB()
+	getDB()
 	card := new(models.Card)
 	if err = c.Bind(card); err != nil {
 		panic(err.Error())
@@ -42,4 +43,19 @@ func CreateCard(c echo.Context) error {
 		panic(err.Error())
 	}
 	return c.JSON(http.StatusOK, card)
+}
+
+func CreateUser(c echo.Context) error {
+	getDB()
+	user := new(models.User)
+	if err = c.Bind(user); err != nil {
+		panic(err.Error())
+	}
+
+	query := "INSERT INTO users(name, email, token, uid, create_at) values(?, ?, ?, ?, NOW())"
+	_, err = db.Exec(query, user.Name, user.Email, user.Token, user.Uid)
+	if err != nil {
+		panic(err.Error())
+	}
+	return c.JSON(http.StatusOK, user)
 }
