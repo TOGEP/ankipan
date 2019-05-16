a5fa6cf9ca7821af6070d989be0bf1124ad506e6
k-nasa <htilcs1115@gmail.com>
2019-05-16 12:05:50 +0900

    fix: dbカラム名が実際のと違うので修正

--- models/card.go
+++ models/card.go
@@ -2,7 +2,7 @@ package models
 
 type Card struct {
   Id int `db:"id" json:"id"`
-  Problem string `db:"id" json:"problem"`
-  Anser string `db:"anser" json:"anser"`
-  Note string `db:"note" json:"note"`
+  Problem string `db:"problex_statement" json:"problem_statement"`
+  Anser string `db:"anser_text" json:"anser_text"`
+  Note string `db:"memo" json:"memo"`
 }
