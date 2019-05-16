e550ab48adee30ccfed6b6e446efbb9ee6916ac2
k-nasa <htilcs1115@gmail.com>
2019-05-16 13:00:03 +0900

    refactor: dbのカラム名にstruct field名を合わせる

--- models/card.go
+++ models/card.go
@@ -3,7 +3,7 @@ package models
 type Card struct {
   Id int `db:"id" json:"id"`
   Problem string `db:"problex_statement" json:"problem_statement"`
-  Anser string `db:"anser_text" json:"anser_text"`
-  Note string `db:"memo" json:"memo"`
+  Anser string `db:"answer_text" json:"answer_text"`
+  Memo string `db:"memo" json:"memo"`
   SolvedCount int `db:"solved_count" json:"solved_count"`
 }
